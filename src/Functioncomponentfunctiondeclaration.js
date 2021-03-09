eb889220f0feeedabb06ab529e1e54a0d84c80ca
Thenmozhirgb <75113380+Thenmozhirgb@users.noreply.github.com>
2021-03-09 22:51:31 +0530

    changes in quotes

--- src/Functioncomponentfunctiondeclaration.js
+++ src/Functioncomponentfunctiondeclaration.js
@@ -2,12 +2,12 @@ import React, {Component} from 'react';
 
 function Functioncomponentfunctiondeclaration(){
      function handleClick() {
-           alert("text message");
+           alert('text message');
         }
         return (
           <div>
               <h1>Function Component Function declaration</h1>
-            <input type="button" onClick={handleClick} value="Click" />
+            <input type='button' onClick={handleClick} value='Click' />
           </div>
         );
       }
